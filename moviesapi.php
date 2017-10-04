2db4427633699d15b0a50bb134847082b69a561c
RonanBurke91 <32508615+RonanBurke91@users.noreply.github.com>
2017-10-04 10:01:08 +0100

    Create moviesapi.php

---
+++ moviesapi.php
@@ -0,0 +1,30 @@
+<?php
+
+   class Movie {
+   
+     public $director;
+     public $title;
+     public $rating;
+     
+    }
+    
+    
+    /*declare your variable */
+    $movies = array ();
+    
+    $movie = new movie();
+    $movie->director = "Christopher Nolan";
+    $movie->title = "Batman";
+    $movie->rating="18";
+    
+    /*populate your array with a list of dummy strings */
+      for($i = 0; $i < 10; $i++)  {
+         $movie = new movie();
+         $movie->director = "Director $i";
+         $movie->title = "title $i";
+         $movie->rating="PG $i";
+         $movies[]=$movie;
+         
+       }
+       
+       header('Content-Type: aoolication/json');
