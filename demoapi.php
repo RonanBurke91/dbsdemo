670ff0571d685980f7eddeda35e06e8c235b69ec
RonanBurke91 <32508615+RonanBurke91@users.noreply.github.com>
2017-10-04 09:35:04 +0100

    Create demoapi.php

---
+++ demoapi.php
@@ -0,0 +1,20 @@
+<?php
+/*Declare your variables*/
+  $items = array();
+/*populate your array with a list of dummy strings */
+   for($i = 0;  $i < 10; $i++){
+   $items[$i] = "this is message $i";
+  }
+  
+  /*set the header type for output*/
+  
+  header('Content-Type: application/json');
+  
+  /*create variable to hold your json data*/
+  
+     $jsonOutput = json_encode($items);
+  
+  /* output the json data* / 
+       
+       echo $jsonOutput;
+   
