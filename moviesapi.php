c354e7a624c574fdee659465692337b6ce1513df
RonanBurke91 <32508615+RonanBurke91@users.noreply.github.com>
2017-10-04 10:19:07 +0100

    Update moviesapi.php

--- moviesapi.php
+++ moviesapi.php
@@ -1,32 +1,42 @@
-<?php
-
-   class Movie {
-   
-     public $director;
-     public $title;
-     public $rating;
-     
-    }
-    
-    
-    /*declare your variable */
-    $movies = array ();
-    
-
-    /*add a movie */
-    $movie = new movie();
-    $movie->director = "Christopher Nolan";
-    $movie->title = "Batman";
+<?php  
+  class Movie {
+    public $director;
+    public $title;
+    public $rating;
+  }
+/* Declare your variables */  
+    $movies = array();  
+// Create a  movie and add it to list
+    $movie = new Movie();
+    $movie->director = "Batman";
+    $movie->title="Christoper Nolan";
     $movie->rating="18";
-    
-    /*populate your array with a list of dummy strings */
-      for($i = 0; $i < 10; $i++)  {
-         $movie = new movie();
-         $movie->director = "Director $i";
-         $movie->title = "title $i";
-         $movie->rating="PG $i";
-         $movies[]=$movie;
-         
-       }
-       
-       header('Content-Type: aoolication/json');
+    $movies[]=$movie;
+// Create a  movie and add it to list
+    $movie = new Movie();
+    $movie->director = "Spiderman";
+    $movie->title="Sam Raimi";
+    $movie->rating="15";
+    $movies[]=$movie;
+//Add a Movie 
+  
+/* Populate your array with a list of dummy strings */  
+    for($i = 0; $i < 10; $i++) {  
+        $movie = new Movie();
+        $movie->director = "Director $i";
+        $movie->title="Title $i";
+        $movie->rating="PG $i";
+        $movies[]=$movie;
+    }  
+  
+/* Set the header type for output */  
+  
+    header('Content-Type: application/json');  
+  
+/* Create a variable to hold your JSON data */  
+  
+    $jsonOutput =  json_encode($movies);  
+  
+/* Output the JSON data */  
+  
+    echo $jsonOutput;
